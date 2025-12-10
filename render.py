190b3bae17a9de3da02e8e45e7968295c663a90c
karin0 <karin0@gmx.com>
2025-12-11 04:59:31 +0900

    more robust but more weird

--- render.py
+++ render.py
@@ -1,4 +1,5 @@
 import re
+import string
 import asyncio
 
 from typing import Any, Iterable, Callable
@@ -299,7 +300,7 @@ async def handle_render_callback(
 
 
 reg = re.compile(r'{(.+?)}|^([^\r\n]+?);$', re.DOTALL | re.MULTILINE)
-all_unary = frozenset('+/$:*`')
+all_punct = frozenset(string.punctuation.replace('-', '').replace('_', ''))
 MAX_DEPTH = 20
 
 
@@ -412,11 +413,18 @@ class RenderContext:
         self, cmd: str, p1: int, p2: int, set: Callable[[str, Value], Any]
     ):
         key1 = cmd[:p1].strip()
-        parts = cmd[p2:].split('=')
-        val = self.evaluate(parts.pop().strip())
+        q = (p := p2) - 1
+        while p < len(cmd):
+            if (c := cmd[p]) == '=':
+                q = p
+            elif c in all_punct:
+                break
+            p += 1
+        val = self.evaluate(cmd[q + 1 :].strip())
         set(key1, val)
-        for key in parts:
-            set(key.strip(), val)
+        if keys := cmd[p2:q].strip():
+            for key in keys.split('='):
+                set(key.strip(), val)
 
     def _repl(self, m: re.Match) -> str:
         cmd: str = m[1] or m[2]
@@ -455,80 +463,33 @@ class RenderContext:
 
             # Empty directive
             if not cmd:
-                pass
+                return
 
             # Literal: {`raw}
-            elif cmd[0] == '`':
+            if cmd[0] == '`':
                 if ret is None:
                     ret = cmd[1:]
                 else:
                     errors.append('redundant literal: ' + cmd)
+                return
 
             # Db name set: {name:}
-            elif cmd[-1] == ':':
+            if cmd[-1] == ':':
                 if self.depth == 0:
                     if self.doc_name:
                         errors.append('doc name redefined: ' + cmd)
                     else:
                         self.doc_name = cmd[:-1].strip()
-
-            # Context override: {key:=value}
-            # a. Can never be overridden (except by markup buttons)
-            # b. Does not interrupt the natural order of flags detected in markup
-            # {key1:=key2=...=value} affects all keys
-            elif (p := cmd.find(':=', 1)) >= 0:
-                self._do_assignments(cmd, p, p + 2, ctx.setdefault_override)
-
-            # Context set if empty: {key?=value}
-            # {key1?=key2=...=value} sets all keys to value if empty
-            elif (p := cmd.find('?=', 1)) >= 0:
-                self._do_assignments(cmd, p, p + 2, ctx.setdefault)
-
-            # Context set: {key=value}
-            # {key1=key2=...=value} sets all keys to value
-            elif (p := cmd.find('=', 1)) >= 0:
-                self._do_assignments(cmd, p, p + 1, ctx.__setitem__)
-
-            # Context inplace replacement: {key|pat1/rep1|pat2/rep2|...}
-            elif (p := cmd.find('|', 1)) >= 0:
-                key = cmd[:p].strip()
-                val = ctx.get(key)
-                if val is None:
-                    return errors.append('undefined: ' + cmd)
-
-                parts = cmd[p + 1 :].split('|')
-                if not parts:
-                    return errors.append('invalid replacements: ' + cmd)
-
-                r = str(val)
-                for part in parts:
-                    try:
-                        pat, rep = part.split('/', 1)
-                    except ValueError:
-                        errors.append('invalid replacement: ' + part)
-                    else:
-                        r = r.replace(pat.strip(), rep.strip())
-
-                ctx[key] = r
-
-            # Context swap: {key1^key2}
-            elif (p := cmd.find('^', 1)) >= 0:
-                key1 = cmd[:p].strip()
-                key2 = cmd[p + 1 :].strip()
-                val1 = ctx.get(key1)
-                val2 = ctx.get(key2)
-
-                self.set_ctx(key1, val2)
-                self.set_ctx(key2, val1)
+                return
 
             # Context flag override: {+key1} means {key1:=1}, {/key2} means {key2:=0}
             # Also starts an unary chain: {+key1/key2/+key3*doc}
             # Other unary operators like `$:*` don't start a chain, as they yields
             # immediate values instead of preparing context.
-            elif cmd[0] == '+' or cmd[0] == '/':
+            if cmd[0] == '+' or cmd[0] == '/':
                 val = '1' if cmd[0] == '+' else '0'
                 for i in range(2, len(cmd)):
-                    if cmd[i] in all_unary:
+                    if cmd[i] in all_punct:
                         key = cmd[1:i].strip()
                         ctx.setdefault_override(key, val)
                         if rest := cmd[i:]:
@@ -537,15 +498,89 @@ class RenderContext:
                 else:
                     key = cmd[1:].strip()
                     ctx.setdefault_override(key, val)
+                return
+
+            for p, c in enumerate(cmd):
+                if c in all_punct:
+                    break
+
+            # p is the first punctuation position, or len(cmd)
+            if 0 < p < len(cmd):
+                match cmd[p : p + 2]:
+                    # Context override: {key:=value}
+                    # a. Can never be overridden (except by markup buttons)
+                    # b. Does not interrupt the natural order of flags detected in markup
+                    # {key1:=key2=...=value} affects all keys
+                    case ':=':
+                        return self._do_assignments(
+                            cmd, p, p + 2, ctx.setdefault_override
+                        )
+
+                    # Context set if empty: {key?=value}
+                    # {key1?=key2=...=value} sets all keys to value if empty
+                    case '?=':
+                        return self._do_assignments(cmd, p, p + 2, ctx.setdefault)
+
+                match cmd[p]:
+                    # Context set: {key=value}
+                    # {key1=key2=...=value} sets all keys to value
+                    case '=':
+                        return self._do_assignments(cmd, p, p + 1, ctx.__setitem__)
+
+                    # Context inplace replacement: {key|pat1/rep1|pat2/rep2|...}
+                    case '|':
+                        key = cmd[:p].strip()
+                        val = ctx.get(key)
+                        if val is None:
+                            return errors.append('undefined: ' + cmd)
+
+                        parts = cmd[p + 1 :].split('|')
+                        if not parts:
+                            return errors.append('invalid replacements: ' + cmd)
+
+                        r = str(val)
+                        for part in parts:
+                            try:
+                                pat, rep = part.split('/', 1)
+                            except ValueError:
+                                errors.append('invalid replacement: ' + part)
+                            else:
+                                r = r.replace(pat.strip(), rep.strip())
+
+                        ctx[key] = r
+                        return
+
+                    # Context swap: {key1^key2}
+                    case '^':
+                        key1 = cmd[:p].strip()
+                        key2 = cmd[p + 1 :].strip()
+                        val1 = ctx.get(key1)
+                        val2 = ctx.get(key2)
+
+                        self.set_ctx(key1, val2)
+                        self.set_ctx(key2, val1)
+                        return
 
             # Only the first expression is evaluated. Subsequent ones are discarded.
-            elif ret is None:
+            if ret is None:
                 ret = self.evaluate(cmd, in_directive=in_directive, as_str=True)
             else:
                 errors.append('redundant expression: ' + cmd)
 
-        for cmd in cmd.split(';'):
-            handle_cmd(cmd.strip())
+        # Find semicolons, but not inside quotes. Very trivial parsing.
+        q0 = q1 = 1
+        last = 0
+        for p, c in enumerate(cmd):
+            match c:
+                case '\'':
+                    q0 ^= 1
+                case '\"':
+                    q1 ^= 1
+                case ';':
+                    if q0 and q1:
+                        handle_cmd(cmd[last:p].strip())
+                        last = p + 1
+        handle_cmd(cmd[last:].strip())
 
         return ret or ''
 
