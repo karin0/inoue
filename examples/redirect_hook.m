4e189b7e69d90543ea73b3a5c3006f6e33a913e1
karin0 <karin0@gmx.com>
2026-04-29 10:56:07 +0900

    examples: update `redirect_hook.m`.

--- examples/redirect_hook.m
+++ examples/redirect_hook.m
@@ -1,38 +1,79 @@
-{
+t0 = "perf";
+
+Redirecting {
   redir:;
 
-  // args
+  // Args. Set them before invoking us.
   text ?= 'Lorem ipsum dolor sit amet.';
   hook ?= 'echo hook: [$(cat -)]';
+  stat ?= 0;
 
   text = "cleanup(text)";
   info = { "bold(len(text))"; ' chars to hook '; "code(hook)" };
+  info;
 
-  rc = { r ⇒
-    body = {
-      'Redirected '; info; ' in ';
-
-      dt = {
-        "r.elapsed >= 1" ? "'%.3fs' % r.elapsed" : "'%dms' % (r.elapsed * 1000)";
-      };
-      "bold(dt)";
-
-      "r.returncode" ? ', status '; "bold(r.returncode)" !;
-      '\n';
-
-      "r.stdout" ? {
-        "r.stderr" ?: 'stdout:\n';
-        "pre(r.stdout)"
-      };
-
-      "r.stderr" ? {
-        "r.stdout" ?: 'stderr:\n';
-        "pre(r.stderr)"
-      }
+  // Start a scope (`@`) to avoid name collisions.
+  time = @ { t ↦
+    r = {
+      "t >= 1" ? "'%.3fs' % t" :
+        "t >= 1e-3" ? "'%.3fms' % (t * 1e3)" :
+          "t >= 1e-6" ? "'%.3fµs' % (t * 1e6)" :
+            "'%.3fns' % (t * 1e9)"
     };
-    "edit_message(body)"
+    "bold(r)"
   };
-  "communicate(hook, text).then(rc)";
 
-  'Redirecting '; info; ' ...'
-}
+  // `⇒` creates "closures" that captures the current scope, which is useful for
+  // async callbacks. However, `↦` would also work here, since we are in the initial
+  // root scope.
+  rc1 = { r ⇒
+    t2 = "perf";
+    times = {
+      "time(t1 - t0)"; ' + '; "time(r.elapsed)"; ' + '; "time(t2 - t1 - r.elapsed)"
+    };
+    'Redirected '; info; ' in '; times; ' = '; "time(t2 - t0)";
+
+    "r.returncode" ? ', status '; "bold(r.returncode)" !;
+    '\n';
+
+    "r.stdout" ? {
+      "r.stderr" ?: 'stdout:\n';
+      "pre(r.stdout)"
+    };
+
+    "r.stderr" ? {
+      "r.stdout" ?: 'stderr:\n';
+      "pre(r.stderr)"
+    };
+  };
+
+  cnt = { stat ? 0 : 2 };
+  view = { msg ⇒
+    "cnt < 3" ? {
+      cnt = "cnt + 1";
+      body;
+      stat ? {
+        t3 = "perf";
+        times = { times; ' + '; "time(t3 - t2)" };
+        t2 = $t3;
+        'Sent '; msg; ' x '; cnt; ' in '; times; ' = '; "time(t3 - t0)"
+      }
+    }
+  };
+
+  rc2 = { body ⇒
+    msg = '';
+    w = { msg ⇒
+      r = "view(msg)";
+      r ? "edit_message(r).then(w)";
+    };
+    *w;
+  };
+
+  "communicate(hook, text).then(rc1, rc2)"
+
+  // The async task is not scheduled until our entire rendering finishes, so we
+  // take the time at the last moment.
+} ...
+
+t1 = "perf";
