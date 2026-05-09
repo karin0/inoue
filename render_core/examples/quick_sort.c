47cea2a4a53f307f389b69a83ad82e45d96a08e0
karin0 <karin0@gmx.com>
2026-05-10 01:51:29 +0900

    render_core: simplify quick_sort example

--- render_core/examples/quick_sort.c
+++ render_core/examples/quick_sort.c
@@ -6,36 +6,29 @@ n = $0;
 
 i = 0;
 { cond ↦ i < n }
-{ step ↦ i += 1 }
+{ step ↦ ++i }
 { body ↦ a[i] = randint(10000) }
 *for;
 
 qs = { @; l, r ↦
-  l<r ? {
+  l < r ? {
     pivot = a[r]
     p = l-1
 
     j = $l
     { cond ↦ j < r }
-    { step ↦ j += 1 }
-    { body ↦
-      val = a[j]
-      val < pivot ? {
-        p = p+1
-        a[p] ^ a[j]
-      }
-    }
+    { step ↦ ++j }
+    { body ↦ a[j] < pivot ? a[++p] ^ a[j] }
     *for
 
-    p = p+1
-    a[p] ^ a[r]
+    a[++p] ^ a[r]
 
     qs(l, p-1)
     qs(p+1, r)
   }
 };
 
-// .l = "0"; .r = "n-1"; *qs;
+// .l = 0; .r = n-1; *qs;
 qs(0, n-1);
 
 i = 0;
